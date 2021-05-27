5d3a9ffe870ead88ad9013e284bdc9327f710f3b
barberrypie <barberryp@gmail.com>
2021-05-27 20:13:42 +0300

    about + delivery

---
+++ src/views/Delivery.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div style="border-bottom:2px solid #F5F5F5">
+    <h2 class = " mx-3 mb-3" style="color: rgba(0,0,0,.7)"> Доставка и оплата</h2>
+  </div>
+  <h3 class = "mx-3 mb-3" style="color: rgba(0,0,0,.7)"> Реквизиты компании</h3>
+  <ul style="list-style: none; color: rgba(0,0,0,.7); text-align: justify ">
+
+      <li>ИНН 3123479798</li>
+      <li>КПП 312301001</li>
+      <li>ОГРН 1213100006921</li>
+      <li>БИК: 044525593</li>
+      <li>Банк: АО «АЛЬФА-БАНК»</li>
+      <li>Кор. счёт: 30101810200000000593</li>
+      <li>Номер счёта: 40702810802970004795</li>
+      <li>Директор: Печинога Галина Ивановна</li>
+
+  </ul>
+  <h3 class = "mx-3 mb-3" style="color: rgba(0,0,0,.7); "> Оформление заказа</h3>
+  <ul style="list-style: none; color: rgba(0,0,0,.7); text-align: justify ">
+    <li>Вы можете оставить заявку, позвонив по телефону <a class="del" href="tel:+79611732393">+7-961-173-23-93</a>, или написав нам на почту <a class="del" href="mailto:forma2393@yandex.ru">forma2393@yandex.ru.</a></li>
+    <li>Доставка осуществляется по всей России.</li>
+  </ul>
+</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d895.7076476405354!2d36.63399346728536!3d50.58170392716581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41266a24e49f2d11%3A0x4e5da6e84b62b1cc!2z0JzQuNGF0LDQudC70L7QstGB0LrQvtC1INGILiwgMjMsIDIwMdCSLCDQkdC10LvQs9C-0YDQvtC0LCDQkdC10LvQs9C-0YDQvtC00YHQutCw0Y8g0L7QsdC7LiwgMzA4MDA2!5e0!3m2!1sru!2sru!4v1622102164128!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
