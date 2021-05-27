ee9a3ce49a59b7aa0a30fac7717d10b6388fe699
barberrypie <barberryp@gmail.com>
2021-05-27 22:51:15 +0300

    product ++

--- src/views/Сonfectionery.vue
+++ src/views/Сonfectionery.vue
@@ -4,33 +4,12 @@
       <h2 class = " mx-3 mb-3" style="color: rgba(0,0,0,.7)">Кондитерские формы</h2>
     </div>
     <div class="container">
-      <div class="row shadow mt-3 rounded">
-        <h3 class = "mb-4 mt-3 " style="color: rgba(0,0,0,.7)">Форма кондитерская "Цыпленок"</h3>
-        <div class="col-md-4 ">
-          <img src="img/konditer.png" class="shadow-sm rounded" alt="..." width="100%" height="auto">
-        </div>
-        <div class="col-md-7 p-0 text-center">
-          <dl class="p-0 mb-4" style="list-style: none; color: rgba(0,0,0,.7); font-size: larger">
-            <dt>Размер одной формы:</dt>
-            <dd>100x70 мм</dd>
-            <dt>Высота:</dt>
-            <dd>50 мм</dd>
-            <dt>Масса готового изделия:</dt>
-            <dd>~0.25 кг</dd>
-            <dt>Материал:</dt>
-            <dd>Алюминий</dd>
-            <dt>Габариты:</dt>
-            <dd>100x70x50 мм</dd>
-            <dt>Толщина стали:</dt>
-            <dd>0.5 мм</dd>
-            <dt>Длина связки:</dt>
-            <dd>-</dd>
-            <dt>Масса формы:</dt>
-            <dd>0.2 кг</dd>
-          </dl>
-        </div>
-
-      </div>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
 
     </div>
 
@@ -38,3 +17,9 @@
 
   </div>
 </template>
+<script>
+import Product from "../components/Product";
+export default {
+  components: {Product}
+}
+</script>
