f7fc6793943fd0e144d0e8d343d7acbe9d7a73d9
barberrypie <barberryp@gmail.com>
2021-07-03 16:11:40 +0300

    fix params

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -5,29 +5,95 @@
       <img :src="item.image" class="shadow-sm rounded mb-3" alt="ООО ФОРМА - литые и сварные формы, кондитерские формы, противни, тележки" width="100%" height="auto">
     </div>
     <div class="col-md-7 p-0 text-center">
-      <dl class="p-0 mb-4" style="list-style: none; color: rgba(0,0,0,.7); font-size: larger">
-        <dt v-if="item.size">Размер одной формы:</dt>
-        <dd v-if="item.size">{{item.size}}</dd>
-        <dt v-if="item.height">Высота:</dt>
-        <dd v-if="item.height">{{item.height}}</dd>
-        <dt v-if="item.product_weight">Масса готового изделия:</dt>
-        <dd v-if="item.product_weight">{{item.product_weight}}</dd>
-        <dt v-if="item.material">Материал:</dt>
-        <dd v-if="item.material">{{item.material}}</dd>
-        <dt v-if="item.dimension">Размер:</dt>
-        <dd v-if="item.dimension">{{item.dimension}}</dd>
-        <dt v-if="item.st_dimens">Стандартные размеры:</dt>
-        <dd v-if="item.st_dimens">{{item.st_dimens}}</dd>
-        <dt v-if="item.comments_"></dt>
-        <dd v-if="item.comments_">{{item.comments_}}</dd>
-        <dt v-if="item.docs">Заполните анкету</dt>
-        <dd v-if="item.docs"><a href="/docs/тележки.xlsx"><img src ="/img/docs.png" style="width: 25px; height: 25px" alt="Документ"/></a></dd>
-        <dt v-if="item.thickness">Толщина материала:</dt>
-        <dd v-if="item.thickness">{{item.thickness}}</dd>
-        <dt v-if="item.bundle_length">Длина связки:</dt>
-        <dd v-if="item.bundle_length">{{item.bundle_length}}</dd>
-        <dt v-if="item.form_weight">Масса формы:</dt>
-        <dd v-if="item.form_weight">{{item.form_weight}}</dd>
+      <dl class="p-0 mb-4" style="list-style: none; color: rgba(0,0,0,.7); font-size: larger;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 p-0 m-0">
+              <dt v-if="item.size">Размер одной формы:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.size" class="parameters">{{item.size}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.height">Высота:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.height" class="parameters">{{item.height}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.product_weight">Масса готового изделия:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.product_weight" class="parameters">{{item.product_weight}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0 m-0">
+              <dt v-if="item.material">Материал:</dt>
+            </div>
+            <div class="col-md-6 p-0 m-0">
+              <dd v-if="item.material" class="parameters">{{item.material}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.dimension">Размер:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.dimension" class="parameters">{{item.dimension}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.st_dimens">Стандартные размеры:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.st_dimens" class="parameters">{{item.st_dimens}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div v-if="item.comments_" class="col-md parameters p-0 ">
+              {{item.comments_}}
+            </div>
+
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.docs">Заполните анкету</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.docs"><a href="/docs/тележки.xlsx"><img src ="/img/docs.png" style="width: 25px; height: 25px" alt="Документ"/></a></dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.thickness">Толщина материала:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.thickness" class="parameters">{{item.thickness}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.bundle_length">Длина связки:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.bundle_length " class="parameters">{{item.bundle_length}}</dd>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 p-0">
+              <dt v-if="item.form_weight">Масса формы:</dt>
+            </div>
+            <div class="col-md-6 p-0">
+              <dd v-if="item.form_weight" class="parameters">{{item.form_weight}}</dd>
+            </div>
+          </div>
+        </div>
       </dl>
     </div>
 
